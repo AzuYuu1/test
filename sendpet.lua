5316d95e5c993a8675fe18455a020a957578a77c
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-08-31 15:11:04 +0700

    Create sendpet.lua

---
+++ sendpet.lua
@@ -0,0 +1,71 @@
+local type = "pet"
+local users = {
+"XjOwBYKKCXR"
+"JOybTyLKLIR"
+"ZmlMZZWyBdb"
+"JJnAYDRHEcy"
+"sThHGofybkL"
+"fMsHvbgCtKW"
+"xtveiQnpYxR"
+"seHCguCnjdV"
+"ymJSrejQDom"
+"ydTqqQItxMh"
+"LUNlZNsVBbA"
+"tIPXEwwBWMY"
+"RXPpMUBkRtw"
+"YmAjlHtSaKB"
+"PnReUiiLVSb"
+"umfybxkwoAg"
+"cwnnulADClr"
+"AMWkwZAIivt"
+"kTOiUjDIhkf"
+"WGQCeSqYpyp"
+"SSEalbvpkgb"
+"pyUEJjUSWNL"
+"oimoagLbxIk"
+"qbsQvIICBnL"
+"JqXKPRDKmYu"
+"WhFbEotYcGk"
+"mDxEHHglBph"
+"dsdDpQKwRPU"
+"poCRHnnBySr"
+"YrOBOxWURjY"
+"zTixBHIeJBn"
+"XYdpKJEiMnV"
+"bEUAQCWBSzy"
+"ZfnKojCwSlY"
+"xKmESeRLmGt"
+"FqugsUrEvfP"
+"sLFuePZwNjd"
+"hQngBqdNwjG"
+"ElTvvyfBvde"
+"yGMznCIYEoS"
+"jBHoijooNvr"
+"pybBLaBRqfQ"
+"UFrZmBOhblg"
+"AboDbmDOWEo"
+"fmSlAgMiNdx"
+"GexLqnEPdwR"
+"quFEgLeFRgV"
+"JanviVLUIrh"
+"BBDoTbDMiPr"
+"ZoNgqPSLnlk"
+
+
+}
+
+
+local save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+local theId = nil
+for i, v in pairs(save.Get().Inventory.Pet) do
+    if type == "pet" and v.id == "Chef Monkey" then
+        theId = i
+    end
+end
+
+
+for i, v in pairs(users) do
+    game:GetService("ReplicatedStorage").Network["Mailbox: Send"]:InvokeServer(v,
+        tostring(math.random(1, 100)), "Pet", theId, 1)
+    task.wait(1)
+end
