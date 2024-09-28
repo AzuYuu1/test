25f77a8ed31478cf0f0d94e5b6663b9db3da7788
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-09-28 12:41:48 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,266 +1,263 @@
-local webhooks = {
-    ["Rare"] = "",
-    ["Epic"] =
-    "https://discord.com/api/webhooks/1247745918132817940/3iz-dKpd8hcJWS6SDqtBZjzADbf80QPnrCaeVM9ZL17ZiQiL2BP6hXeTaYKCaFyk5yus",
-    ["Legendary"] =
-    "https://discord.com/api/webhooks/1247746014849269811/rJoQkIFhQowY0nTh-S7Lfxte-oBibj5YSrW_k6CyHgSgO9f6jpWG90hqI56EPJ0xlWLq",
-    ["Mythic"] =
-    "https://discord.com/api/webhooks/1247746549921087509/03K6KobirF9kHjohQrHbZjYsWCHJ3VvEi_0kUfy-W4KJ6cPfT_cLoIjUXUVuZFhVmQ4w",
-    ["Secret"] =
-    "https://discord.com/api/webhooks/1247746617780731914/8ue96sjIFmHFnan0Hh2bIW_J-TQvP5MXLR5y_RrJ14Y43_LiFke9VgnCofsY-aukjHoP",
+local codes = {
+    "TIKTOK50K",
+    "ROST10K",
+    "SLAYER",
+    "LATEUPDATESORRY"
+}
+
+local autoDeleteRarity = {
+    "Rare",
 }
 
 local notifyIfRarity = {
-    ["Rare"] = false,
-    ["Epic"] = false,
-    ["Legendary"] = false,
-    ["Mythic"] = false,
-    ["Secret"] = false,
+    "Epic",
+    "Legendary",
+    "Mythic",
+    "Secret"
 }
-local bannerName = "Standard"
 
-local sellOfRarity = {
-    ["Rare"] = true,
-    ["Epic"] = true,
-    ["Legendary"] = true,
-    ["Mythic"] = false,
-    ["Secret"] = false,
+local webhooks = {
+    ["Epic"] = "https://discord.com/api/webhooks/1277944088728961084/x8E5vTzZ5xCxRrzXbI5ykOZ46lj4KQZPMEVGR8EuhBpq7xxHWRavCNdsij_MZ99EkE8r",
+    ["Legendary"] = "https://discord.com/api/webhooks/1277944088728961084/x8E5vTzZ5xCxRrzXbI5ykOZ46lj4KQZPMEVGR8EuhBpq7xxHWRavCNdsij_MZ99EkE8r",
+    ["Mythic"] = "https://discord.com/api/webhooks/1276076987437289564/nGbqKiWhsIInnzPE0j79rCCuKFS7S7YAu4Ia2-RQL1BWBkFzFJS-gGMMtbUP0yJq4uyQ",
+    ["Secret"] = "https://discord.com/api/webhooks/1276076911738753074/rYYm1C5-btuAF9tjTzHQr0NrysCvgrIWE8X0DxE4XG5wbsCViO0wP0KCCYlVOLEevafU"
 }
 
 
-local VirtualInputManager = game:GetService("VirtualInputManager")
-local function clickGui(gui, offsetY)
-    local width, height = gui.AbsoluteSize.X, gui.AbsoluteSize.Y
-    local x, y = gui.AbsolutePosition.X, gui.AbsolutePosition.Y
-    local centerX, centerY = x + width / 2, y + height / 2
-    VirtualInputManager:SendMouseButtonEvent(centerX, centerY + offsetY, 0, true, game, 0)
-    VirtualInputManager:SendMouseButtonEvent(centerX, centerY + offsetY, 0, false, game, 0)
-    print("Clicking")
-end
+local summonCFrame = CFrame.new(-377.539185, 50.9425964, 746.048828, -0.977286458, -5.76542156e-08, -0.211922646,
+    -4.38978809e-08, 1, -6.96169593e-08, 0.211922646, -5.8732752e-08, -0.977286458)
+local units = game:GetService("Players").LocalPlayer.PlayerGui.Windows.Units.Holder.Main.Units
 
-if game.Players.LocalPlayer.PlayerGui.PAGES.SettingsUI.ListFrame["Settings Auto Sell Deletes Shinies"].Frame.BooleanButton.StatusLabel.Text == "Off" then
-    clickGui(game:GetService("Players").LocalPlayer.PlayerGui.TopBar.HolderFrame.Left.dropDown, 25)
-    wait(0.3)
-    clickGui(game.Players.LocalPlayer.PlayerGui.TopBar.HolderFrame.Left.dropDown.DropdownFrame.Settings, 25)
-    local oldLayout = game.Players.LocalPlayer.PlayerGui.PAGES.SettingsUI.ListFrame
-        ["Settings Auto Sell Deletes Shinies"].LayoutOrder
+repeat wait(1) until game:IsLoaded()
 
-    game.Players.LocalPlayer.PlayerGui.PAGES.SettingsUI.ListFrame["Settings Auto Sell Deletes Shinies"].LayoutOrder = 1
-    wait(0.3)
-    clickGui(
-        game.Players.LocalPlayer.PlayerGui.PAGES.SettingsUI.ListFrame["Settings Auto Sell Deletes Shinies"].Frame
-        .BooleanButton, 25)
-    wait(0.3)
-    game.Players.LocalPlayer.PlayerGui.PAGES.SettingsUI.ListFrame["Settings Auto Sell Deletes Shinies"].LayoutOrder =
-        oldLayout
-    wait(0.3)
-    clickGui(game.Players.LocalPlayer.PlayerGui.PAGES.SettingsUI.CloseButton, 25)
-end
-
------------------ ANTI-CHEAT BYPASS ----------------- ANTI-CHEAT BYPASS ----------------- start
-local Remote_Manager = {}
-
-Remote_Manager.SafeEnvironment = nil
-
-Remote_Manager.FireSafeEnvironment = function(remoteName, args)
-    repeat task.wait() until Remote_Manager.SafeEnvironment
-    _G.A = remoteName
-    _G.B = args
-    _G.TYPE = "Fire"
-    repeat task.wait() until not _G.A and not _G.B and not _G.TYPE
-end
-
-Remote_Manager.InvokeSafeEnvironment = function(remoteName, args)
-    repeat task.wait() until Remote_Manager.SafeEnvironment
-    _G.A = remoteName
-    _G.B = args
-    _G.TYPE = "Invoke"
-    repeat task.wait() until not _G.A and not _G.B and not _G.TYPE
-end
+--#region funcs
 
 
-Remote_Manager.Init = function()
-    setthreadcontext(2)
+local unitHandler = require(game:GetService("StarterPlayer").Modules.Interface.Loader.Windows.UnitWindowHandler)
 
-    local RS = game:GetService("ReplicatedStorage")
-    local a = require(RS:WaitForChild("Actions"))
-
-
-    local c = require(game:GetService("ReplicatedStorage").SupportModules.Ping)
-    local old = c.GetPlayerPing
-    c.GetPlayerPing = function(...)
-        if not Remote_Manager.SafeEnvironment then
-            Remote_Manager.SafeEnvironment = true
-            old(...)
-            print("get env: ", i)
-            while task.wait() do
-                repeat task.wait() until _G.A and _G.B and _G.TYPE
-                local a_, b, type = _G.A, _G.B, _G.TYPE
-                if type == "Fire" then
-                    a[a_]:Fire(table.unpack(b))
-                else
-                    a[a_]:Invoke(table.unpack(b))
-                end
-                task.wait(.5)
-                _G.A = nil
-                _G.B = nil
-                _G.TYPE = nil
-            end
-        else
-            return old(...)
-        end
-    end
-end
-
-
-task.spawn(Remote_Manager.Init)
------------------ ANTI-CHEAT BYPASS ----------------- ANTI-CHEAT BYPASS ----------------- end
-
--- wait for game load
-local RS = game:GetService("ReplicatedStorage")
-local plrControl = require(game.ReplicatedFirst.Classes.PlayerControl)
-
-if plrControl.LocalPlayer == nil then
-    plrControl.LocalPlayerLoaded.Event:Wait()
-end
-
-repeat task.wait(0.1) until game:IsLoaded()
-
-task.wait(3)
-repeat wait() until Remote_Manager.SafeEnvironment
-print("Loaded")
-
---game:GetService("ReplicatedStorage").Remotes.FirstTimeSummon:InvokeServer()
-
-
--- redeem code
---for i, v in codes do
---game:GetService("ReplicatedStorage").Remotes.UseCode:InvokeServer(v)
---task.wait(0.1)
---end
-
-
--- changed to tickets
-
-spawn(function()
-    Remote_Manager.FireSafeEnvironment("UNIT_EQUIP_BEST", {})
-    while wait(10) do
-        Remote_Manager.FireSafeEnvironment("UNIT_EQUIP_BEST", {})
-    end
-end)
-
-print("Loading")
-local rarityConvert = {
-    [2] = "Rare",
-    [3] = "Epic",
-    [4] = "Legendary",
-    [5] = "Mythic",
-    [6] = "Secret"
-}
-
-function save()
-    return game:GetService("ReplicatedStorage").Remotes.GetInventory:InvokeServer()
-end
-
--- check inventory for troops of rarity
-local UnitData = require(game:GetService("ReplicatedStorage").Modules.Bins.UnitData)
-local unitDataV2 = {}
-
-for i, v in UnitData do
-    unitDataV2[i] = v.Rarity
-end
-
--- get gem using UI 
-local gems = game:GetService("Players").LocalPlayer.PlayerGui.HUD.Toolbar.CurrencyList.Gems.TextLabel
-
-function getGems()
-    return tonumber(gems.Text)
-end
-
-print("Setting auto delete")
-if tostring(game.Players.LocalPlayer.PlayerGui.PAGES.SummonPage.Holder.HolderAutoSell.DefaultButton.AutoDelete_Rare.UIGradient.Color) ~= "0 1 0.454902 0.290196 0 1 1 0.054902 0.0705882 0 " then
-    for i = 2, 4 do
-        print("Firing!")
-        Remote_Manager.FireSafeEnvironment("AUTO_DELETE_RARITY", { tostring(i) })
-    end
-end
-
-print("Selling troops")
-local sellTable = {}
-for i, v in save().Units do
-    local rarity = rarityConvert[unitDataV2[v.Type]]
-    if sellOfRarity[rarity] then
-        table.insert(sellTable, i)
-    end
-end
-print("firing remote to sell all troop")
-
-spawn(function()
-Remote_Manager.FireSafeEnvironment("UNIT_SELL", { sellTable })
-    while wait(1) do
-Remote_Manager.FireSafeEnvironment("UNIT_SELL", { sellTable })
-end
-end)
-
-print("Buying")
--- summon till you cant afford
-repeat
-    print("Buying 10 by 10")
-    -- [detected] game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("Standard", 1)
-    spawn(function()
-        Remote_Manager.InvokeSafeEnvironment("SUMMON", { bannerName, 10 })
-    end)
-    wait(0.5)
-until getGems() < 100
-
-
-repeat
-    print("buying 1 by 1")
-    spawn(function()
-        Remote_Manager.InvokeSafeEnvironment("SUMMON", { bannerName, 1 })
-    end)
-until getGems() < 10
-
-
-function webhook(url, data)
+function notify(data, url)
     pcall(function()
         local newdata = game:GetService("HttpService"):JSONEncode(data)
-
         local headers = {
             ["content-type"] = "application/json"
         }
-
         request = http_request or request or HttpPost or syn.request
         local a = { Url = url, Body = newdata, Method = "POST", Headers = headers }
-
         request(a)
     end)
 end
 
-for i, v in save().Units do
-    local rarity = rarityConvert[unitDataV2[v.Type]]
+function sellRare()
+    local idToName = {}
 
-    -- send webhook if got troop
-    if notifyIfRarity[rarity] then
-        webhook(
-            webhooks[rarity],
-            {
-                ["content"] = game.Players.LocalPlayer.Name .. " rolled a rare troop",
-                ["username"] = "Anime defender",
-                ["embeds"] = {
-                    {
-                        ["title"] = game.Players.LocalPlayer.Name ..
-                            " got a: " .. rarity .. " " .. v.Type,
-                        ["type"] = "rich",
-                        ["color"] = 8388736,
-                        ["author"] = {
-                            ["name"] = game.Players.LocalPlayer.Name,
-                        }
+    for i, v in next, game:GetService("Players").LocalPlayer.PlayerGui.Windows.Units.Holder.Main.Units:GetChildren() do
+        pcall(function()
+            idToName[v.Name] = v.Holder:FindFirstChildOfClass("UIGradient").Name
+        end)
+    end
 
-                    }
+
+    function getUnits()
+        return unitHandler._Cache
+    end
+
+    local sellTable = {}
+
+    for i, v in next, getUnits() do
+        local rarity = idToName[i]
+        if rarity and rarity == "Rare" then
+            table.insert(sellTable, i)
+        end
+    end
+
+    game:GetService("ReplicatedStorage").Networking.Units.SellEvent:FireServer(sellTable)
+end
+
+function summon(type)
+    game:GetService("ReplicatedStorage").Networking.Units.SummonEvent:FireServer(type, 'Special')
+    sellRare()
+end
+
+function doStarterDialog()
+    local fileName = game.Players.LocalPlayer.Name .. "doneDialog" .. ".txt"
+    if isfile(fileName) then return end
+
+    local args = {
+        [1] = "Interact",
+        [2] = {
+            [1] = "StarterUnitDialogue",
+            [2] = 2,
+            [3] = "Yeah!"
+        }
+    }
+
+    game:GetService("ReplicatedStorage").Networking.State.DialogueEvent:FireServer(unpack(args))
+
+
+    local args = {
+        [1] = "Select",
+        [2] = "Roku"
+    }
+
+    game:GetService("ReplicatedStorage").Networking.Units.UnitSelectionEvent:FireServer(unpack(args))
+
+    writefile(fileName, "")
+end
+
+function enableAutoSell(rarity)
+    local fileName = game.Players.LocalPlayer.Name .. "setRarity" .. rarity .. ".txt"
+    if isfile(fileName) then return end
+
+    game:GetService("ReplicatedStorage").Networking.Settings.SettingsEvent:FireServer('Toggle', rarity)
+
+    writefile(fileName, "")
+end
+
+function getGems()
+    local text = game:GetService("Players").LocalPlayer.PlayerGui.HUD.Main.Currencies.Gems.Gems.Text
+
+    text = text:gsub(",", "")
+    text = tonumber(text)
+
+    return text
+end
+
+--#endregion
+
+doStarterDialog()
+
+for i, v in next, codes do
+    game:GetService("ReplicatedStorage").Networking.CodesEvent:FireServer(v)
+
+    wait(0.5)
+end
+
+wait(1)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = summonCFrame
+
+wait(0.5)
+game:GetService("ReplicatedStorage").Networking.ClientListeners.TutorialEvent:FireServer('SaveTutorial', "SummonTutorial")
+game:GetService("ReplicatedStorage").Networking.ClientListeners.TutorialEvent:FireServer('ClaimTutorial',
+    "SummonTutorial")
+
+wait(0.5)
+
+for i, v in next, autoDeleteRarity do
+    enableAutoSell(v)
+    wait(0.3)
+end
+
+-- claim all
+
+local args = {
+    [1] = "ClaimAll"
+}
+
+game:GetService("ReplicatedStorage").Networking.Units.CollectionEvent:FireServer(unpack(args))
+
+
+units.ChildAdded:Connect(function(child)
+    child:WaitForChild("Glow")
+    child:WaitForChild("Holder")
+    local rarity = child.Glow:FindFirstChildOfClass("UIGradient").Name
+    local name = child.Holder.Main.UnitName.Text
+
+    if table.find(notifyIfRarity, rarity) then
+        local data = {
+            ["content"] = game.Players.LocalPlayer.Name .. " summoned unit",
+            ["username"] = "notify",
+            ["embeds"] = {
+                {
+                    ["title"] = name,
+                    ['description'] = "got " .. rarity .. " unit " .. name,
+                    ["type"] = "rich",
+                    ["color"] = 8388736
                 }
-            })
+            }
+        }
+        notify(data, webhooks[rarity])
+    end
+end)
+
+while getGems() >= 500 do
+    summon('SummonTen')
+    wait(0.5)
+end
+
+while getGems() >= 50 do
+    summon('SummonOne')
+    wait(0.5)
+end
+
+
+local unitPath = game:GetService("Players").LocalPlayer.PlayerGui.Windows.Units.Holder.Main.Units
+local units = {}
+
+local rarityDictator = {
+    ["Rare"] = 1,
+    ["Epic"] = 2,
+    ["Legendary"] = 3,
+    ["Mythic"] = 4,
+    ["Secret"] = 5,
+}
+
+-- Function to sort units based on rarity and level
+function compareUnits(a, b)
+    -- Compare rarity first
+    if rarityDictator[a.rarity] ~= rarityDictator[b.rarity] then
+        return rarityDictator[a.rarity] > rarityDictator[b.rarity]
+    else
+        -- If rarity is the same, compare levels
+        return a.level > b.level
+    end
+end
+
+function getBestMount()
+    units = {}
+
+    -- Loop through each unit in the unitPath
+    for i, v in next, unitPath:GetChildren() do
+        if not v:FindFirstChild("Holder") then continue end
+        local rarity = v.Holder:FindFirstChildOfClass("UIGradient").Name
+        local level = tonumber(v.Holder.Main.LevelFrame.Level.Text)
+        local name = v.Holder.Main.UnitName.Text
+        local id = v.Name -- ID is the name of the instance
+
+        -- Store the unit details in the units table
+        table.insert(units, { name = name, level = level, rarity = rarity, id = id })
+    end
+
+    -- Sort the units by rarity and level
+    table.sort(units, compareUnits)
+
+    -- Return the sorted list of units
+    return units
+end
+
+local unlockedN = 0
+function getMaxUnlockedSlots()
+    unlockedN = 0
+    for i, v in next, game:GetService("Players").LocalPlayer.PlayerGui.HUD.Main.Units:GetChildren() do
+        if not v:IsA("Frame") then continue end
+        if not v:FindFirstChild("Locked") or v:FindFirstChild("Locked") and not v.Locked.Visible then
+            unlockedN = unlockedN + 1
+        end
+    end
+    return unlockedN
+end
+
+local max = getMaxUnlockedSlots()
+local equipped = 0
+
+for i, v in next, getBestMount() do
+    if equipped < max then
+        game:GetService("ReplicatedStorage").Networking.Units.EquipEvent:FireServer("Equip", v.id)
+
+        equipped = equipped + 1
     end
 end
 
 
+
+writefile(game.Players.LocalPlayer.Name .. '.txt', "Yummytool")
